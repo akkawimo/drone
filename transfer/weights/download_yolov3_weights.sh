1deddabaa1e7076256509a6fd41fe45347316f4f
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-08-23 21:06:38 +0000

    updated weights to download .ptt

--- transfer/weights/download_yolov3_weights.sh
+++ transfer/weights/download_yolov3_weights.sh
@@ -4,17 +4,20 @@
 mkdir -p weights && cd weights
 
 # copy darknet weight files, continue '-c' if partially downloaded
-#wget -c https://pjreddie.com/media/files/yolov3.weights
-#wget -c https://pjreddie.com/media/files/yolov3-tiny.weights
 wget -c https://pjreddie.com/media/files/yolov3-spp.weights
 
 # yolov3 pytorch weights
-# download from Google Drive: https://drive.google.com/drive/folders/1uxgUBemJVw9wZsdpboYbzUN4bcRhsuAI
+
+# Set fileid and filename
+filename="yolov3-spp.pt"
+fileid="188pvdV_-PPNt34pPkBZDuMHueBOTA_Fc"  # yolov3-spp.pt
+
+# Download from Google Drive, accepting presented query
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+rm ./cookie
+
 
 # darknet53 weights (first 75 layers only)
 wget -c https://pjreddie.com/media/files/darknet53.conv.74
-
-# yolov3-tiny weights from darknet (first 16 layers only)
-# ./darknet partial cfg/yolov3-tiny.cfg yolov3-tiny.weights yolov3-tiny.conv.15 15
-# mv yolov3-tiny.conv.15 ../
 

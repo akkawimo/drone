8c9806e0ef221938db18f615dcf26d5b432ea758
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-08-23 21:51:06 +0000

    transfer learning is working

--- transfer/weights/download_yolov3_weights.sh
+++ transfer/weights/download_yolov3_weights.sh
@@ -9,8 +9,17 @@
 # yolov3 pytorch weights
 
 # Set fileid and filename
-filename="yolov3-spp.pt"
-fileid="188pvdV_-PPNt34pPkBZDuMHueBOTA_Fc"  # yolov3-spp.pt
+filename="yolov3-spp.pt" #needed for transfer learning
+fileid="1NPxUb_hzk2nac2iO8-CVXEcGSOiSFvI6"  # yolov3-spp.pt
+
+# Download from Google Drive, accepting presented query
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+rm ./cookie
+
+# Set fileid and filename
+filename="yolov3.pt" #needed for transfer learning
+fileid="1NPxUb_hzk2nac2iO8-CVXEcGSOiSFvI6"  # yolov3.pt
 
 # Download from Google Drive, accepting presented query
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
